aa808c312f59f93e60b00b2ed2957ad4075a027d
JosAhl <josefineahlstrand@live.se>
2024-10-05 17:43:35 +0200

    Added distance to nav

--- header.php
+++ header.php
@@ -1,16 +1,20 @@
+<?php
+$title = "Some Teams";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Some Teams</title>
+    <title><?= $title ?></title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <header>
-        <h1>Some Teams</h1>
+    <header class="header">
+        <h1><?= $title ?></h1>
         <nav>
             <a href="start.php">Start</a>
             <a href="about.php">About</a>
