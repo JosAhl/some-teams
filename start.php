158089cba49b6a70d92bed914417f9a1ed472c12
JosAhl <josefineahlstrand@live.se>
2024-10-05 11:19:28 +0200

    Fixed link

--- start.php
+++ start.php
@@ -9,13 +9,12 @@ include __DIR__ . "/data.php";
     <div class="grid-container">
         <?php foreach ($teams as $teamName => $teamDetails): ?>
             <article class="grid-item">
-                <h2><?= $teamName; ?></h2>
+                <h2><a href="<?= $teamDetails['url']; ?>"><?= $teamName; ?></a></h2>
                 <img src="<?= $teamDetails['logo']; ?>" alt="<?= $teamName; ?> logo">
                 <p><strong>League:</strong> <?= $teamDetails['league']; ?></p>
                 <p><strong>UEFA Coefficient Ranking:</strong> <?= $teamDetails['uefa-coefficient-ranking']; ?></p>
                 <p><strong>City:</strong> <?= $teamDetails['city']; ?></p>
                 <p><strong>Group:</strong> <?= $teamDetails['group']; ?></p>
-                <p><a href="<?= $teamDetails['url']; ?>">Visit official website</a></p>
             </article>
         <?php endforeach; ?>
     </div>
