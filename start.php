02898e3c9205110a62f90cdfcfc0f43acf8b469a
JosAhl <josefineahlstrand@live.se>
2024-10-05 11:12:35 +0200

    Added teams to start.php

--- start.php
+++ start.php
@@ -1,7 +1,25 @@
 <?php
 require_once __DIR__ . "/header.php";
+
+include __DIR__ . "/data.php";
+
 ?>
 
+<main>
+    <div class="grid-container">
+        <?php foreach ($teams as $teamName => $teamDetails): ?>
+            <article class="grid-item">
+                <h2><?= $teamName; ?></h2>
+                <img src="<?= $teamDetails['logo']; ?>" alt="<?= $teamName; ?> logo">
+                <p><strong>League:</strong> <?= $teamDetails['league']; ?></p>
+                <p><strong>UEFA Coefficient Ranking:</strong> <?= $teamDetails['uefa-coefficient-ranking']; ?></p>
+                <p><strong>City:</strong> <?= $teamDetails['city']; ?></p>
+                <p><strong>Group:</strong> <?= $teamDetails['group']; ?></p>
+                <p><a href="<?= $teamDetails['url']; ?>">Visit official website</a></p>
+            </article>
+        <?php endforeach; ?>
+    </div>
+</main>
 </body>
 
 </html>
