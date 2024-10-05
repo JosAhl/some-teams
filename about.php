ab58c97bc27e17a3b7869572382cd6065c6bbbc3
JosAhl <josefineahlstrand@live.se>
2024-10-05 14:25:40 +0200

    Fixed about.php sizes

--- about.php
+++ about.php
@@ -3,8 +3,15 @@ require_once __DIR__ . "/header.php";
 
 ?>
 
-<main>
-    <p>About me text goes here</p>
+<main class="about">
+    <article class="about-content">
+        <p>My name is Josefine and I was assigned to design this webpage.<br><br>
+            As I know nothing about soccer, I knew from the beginning that I'd have to face this challenge head on.</p>
+        <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTY1aDd2YXlyNmR2d3k3Nmd2ZG9sZGVvdnh4OGI2OGM3cmczaXhpaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KnSm1GhuvUjFm/giphy.webp" alt="Goalkeeper gif">
+        <p>However, I'm here to display information, not to ask questions.<br><br>
+            I'm a developer in the early making who got this assignment mainly to improve my workflow regarding GitHub, deployment and coding.<br><br>
+            I am enthusiastic and willing to keep learning and have high ambition while maintaining achievable goals.</p>
+    </article>
 </main>
 
 <?php
