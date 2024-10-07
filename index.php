a1417b2cf9b8301ae3caeea97d9d2759cec84e78
JosAhl <josefineahlstrand@live.se>
2024-10-07 13:30:29 +0200

    Changed link to grid cell

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ include __DIR__ . "/data.php";
 <main>
     <div class="grid-container">
         <?php foreach ($teams as $teamName => $teamDetails): ?>
-            <article class="grid-item">
-                <h2><a href="<?= $teamDetails['url']; ?>"><?= $teamName; ?></a></h2>
-                <img src="<?= $teamDetails['logo']; ?>" alt="<?= $teamName; ?> logo">
-                <p><strong>League:</strong> <?= $teamDetails['league']; ?></p>
-                <p><strong>City:</strong> <?= $teamDetails['city']; ?></p>
-                <p><strong>UEFA Ranking:</strong> <?= $teamDetails['uefa-coefficient-ranking']; ?></p>
-                <p><strong>Group:</strong> <?= $teamDetails['group']; ?></p>
-            </article>
+            <a href="<?= $teamDetails['url']; ?>" class="grid-link">
+                <article class="grid-item">
+                    <h2><?= $teamName; ?></h2>
+                    <img src="<?= $teamDetails['logo']; ?>" alt="<?= $teamName; ?> logo">
+                    <p><strong>League:</strong> <?= $teamDetails['league']; ?></p>
+                    <p><strong>City:</strong> <?= $teamDetails['city']; ?></p>
+                    <p><strong>UEFA Ranking:</strong> <?= $teamDetails['uefa-coefficient-ranking']; ?></p>
+                    <p><strong>Group:</strong> <?= $teamDetails['group']; ?></p>
+                </article>
+            </a>
         <?php endforeach; ?>
     </div>
 </main>
