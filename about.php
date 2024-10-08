a5a40239cb0fd4486370ff9a22dbf95269831263
JosAhl <josefineahlstrand@live.se>
2024-10-08 20:43:50 +0200

    Changed about content

--- about.php
+++ about.php
@@ -8,9 +8,8 @@ require_once __DIR__ . "/header.php";
         <p>My name is Josefine and I was assigned to design this webpage.<br><br>
             As I know nothing about soccer, I knew from the beginning that I'd have to face this challenge head on.</p>
         <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTY1aDd2YXlyNmR2d3k3Nmd2ZG9sZGVvdnh4OGI2OGM3cmczaXhpaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/KnSm1GhuvUjFm/giphy.webp" alt="Goalkeeper gif">
-        <p>However, I'm here to display information, not to ask questions.<br><br>
-            I'm a developer in the early making who got this assignment mainly to improve my workflow regarding GitHub, deployment and coding.<br><br>
-            I am enthusiastic and willing to keep learning and have high ambition while maintaining achievable goals.</p>
+        <p>I am a developer in the early making who is eager to explore the field while maintaining achievable goals and gradually build my knowledge along the way.<br><br>
+            I got this assignment mainly to improve my workflow regarding GitHub, deployment and coding.</p>
         <div class="contact-links">
             <a href="https://github.com/JosAhl"><img src="https://img.icons8.com/?size=100&id=12599&format=png&color=000000" alt="Github Icon" /></a>
             <a href="mailto:josefineahlstrand@live.se"><img src="https://img.icons8.com/?size=100&id=124379&format=png&color=000000" alt="Email Icon" /></a>
